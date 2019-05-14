a7b23c8659bb661af77d65ccb3f5923bec61f93f
Matthew Peters <matthewpeters@MacBook-Pro-3.local>
2019-05-13 21:36:12 -0400

    Updates

---
+++ precache-manifest.9ae27f092f1f79334c0f8c46c0828dd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04263831fac1b22163a10ae2603d2504",
+    "url": "/birthday/index.html"
+  },
+  {
+    "revision": "8e889faf17e1db5092f9",
+    "url": "/birthday/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/birthday/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "8e889faf17e1db5092f9",
+    "url": "/birthday/static/js/main.6c6d4e6a.chunk.js"
+  },
+  {
+    "revision": "fb2cde24ba4de20a670e",
+    "url": "/birthday/static/js/runtime~main.54c049e0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/birthday/static/media/logo.5d5d9eef.svg"
+  }
+]);
